5cc68f3dd38a6d5bc4b4c9bc4ca3a00beb304bc4
shaaanuu <monshan50@gmail.com>
2026-05-26 23:26:06 +0530

    add: color pallette

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bg = Color(0xFF1C1C1C);
+  static const text = Color(0xFFE5E5E5);
+  static const text75 = Color(0xBFE5E5E5);
+}
